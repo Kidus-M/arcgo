c53eb030e662bfe7ce0e4cc801fdaaccc8f298fe
kidus-m <kidus0237@gmail.com>
2025-12-03 20:43:48 +0300

    repo done

---
+++ Tests/repositories_integration/task_repository_integration_test.go
@@ -0,0 +1,11 @@
+package repositories_integration_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTaskRepositoryIntegrationPlaceholder(t *testing.T) {
+	assert.True(t, true)
+}
---
+++ Tests/repositories_integration/user_repository_integration_test.go
@@ -0,0 +1,11 @@
+package repositories_integration_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRepositoryIntegrationPlaceholder(t *testing.T) {
+	assert.True(t, true)
+}
